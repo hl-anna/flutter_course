243405145733215fd4f8231bb8a2c997a5c280c2
Anna <ahledikova123@gmail.com>
2021-11-07 21:07:12 +0000

    splitting code into files by widgets

--- main.dart
+++ main.dart
@@ -1,36 +1,52 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+import './answer.dart';
+
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  void answerQuestion() {
-    print('Answer chosen!');
+// void main() => runApp(MyApp()); <- a shorter version of the above
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      //how do I actually get access to questions.length?? so i don't have to hardcode this
+      if (_questionIndex < 2) {
+        _questionIndex += 1;
+      } else {}
+    });
+    print(_questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favourite colour?',
+      'What\'s your favourite animal?',
+      'How are you today?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('My First App'),
+          title: Text('Questionnaire'),
         ),
         body: Column(
           children: [
-            Text('The question'),
-            ElevatedButton(
-              child: Text('Answer 1'),
-              onPressed: answerQuestion,
-            ),
-            ElevatedButton(
-              child: Text('Answer2'),
-              onPressed: answerQuestion,
-            ),
-            ElevatedButton(
-              child: Text('Answer3'),
-              onPressed: () => print('Answer3 chosen!'),
-            ),
+            Question(questions[_questionIndex]),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
           ],
         ),
       ),
