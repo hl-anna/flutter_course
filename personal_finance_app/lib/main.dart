217e4fcaa67f208c4f67f67355c10b8474c6c307
Anna Hledikova <>
2022-04-23 18:24:36 +0100

    change visuals, fix day order in chart

--- personal_finance_app/lib/main.dart
+++ personal_finance_app/lib/main.dart
@@ -26,6 +26,7 @@ class MyApp extends StatelessWidget {
                     fontFamily: 'OpenSans',
                     fontSize: 16,
                     fontWeight: FontWeight.bold),
+                button: TextStyle(color: Colors.white),
               ),
           appBarTheme: AppBarTheme(
             textTheme: ThemeData.light().textTheme.copyWith(
